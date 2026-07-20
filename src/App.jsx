b3d478a8dc89967d70471779f692ee2a83fdaaef
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-07-20 20:39:37 -0300

    Cree la estructura inicial e instale dependencies

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer"
+import FormularioTarea from "./components/FormularioTarea"
+
+const App = () => {
+  return (
+    /* Fragment */
+    <>
+    <main className="container my-4">
+      <h1 className="text-center">Lista de tareas</h1>
+      <FormularioTarea/>
+    </main>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/FormularioTarea.jsx
@@ -0,0 +1,11 @@
+
+
+const FormularioTarea = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FormularioTarea
