42fa87c36fdda72e6c5ca0631f32e62e62b97166
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-07-20 21:08:30 -0300

    Cree maquetado formulario

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,10 +1,18 @@
-
+import { Button, Form } from "react-bootstrap"
+import ListaTarea from "./ListaTarea"
 
 const FormularioTarea = () => {
   return (
-    <div>
-      
-    </div>
+    <section>
+       <Form >
+         <Form.Group className="mb-3 d-flex justify-content-between">
+            <Form.Control type="text" placeholder="Ingrese una tarea"/>
+            <Button variant="primary" type="submit">➕</Button>
+         </Form.Group>
+         <Form.Text className="text-danger"></Form.Text>
+      </Form>
+      <ListaTarea/>
+    </section>
   )
 }
 
--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,11 +1,12 @@
-
+import { Button } from 'react-bootstrap';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const ItemTarea = () => {
   return (
-    <div>
-      
-    </div>
-  )
+      <ListGroup.Item className='d-flex justify-content-between'>
+        <Button variant='secondary'>❌</Button>
+      </ListGroup.Item>
+  );
 }
 
 export default ItemTarea
